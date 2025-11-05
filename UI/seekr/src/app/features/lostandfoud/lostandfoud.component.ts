f4dbe382d961b6c3733e67867ca23bc4a3823564
Dhayanand-NS <dhayanandns70@gmail.com>
2025-11-05 08:24:27 +0530

    feat : Added modal for getting requirement from the user(eg : lost or found anything)

--- UI/seekr/src/app/features/lostandfoud/lostandfoud.component.ts
+++ UI/seekr/src/app/features/lostandfoud/lostandfoud.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { MapComponent } from "../../shared/map/map.component";
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { MapComponent } from '../../shared/map/map.component';
 import { LostandfoudService } from '../services/lostandfoud.service';
 import { Coordinates } from '../models/coordinates.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lostandfoud',
-  imports: [MapComponent],
+  imports: [MapComponent,FormsModule, CommonModule],
   templateUrl: './lostandfoud.component.html',
-  styleUrl: './lostandfoud.component.css'
+  styleUrl: './lostandfoud.component.css',
 })
 export class LostandfoudComponent implements OnInit {
-  constructor(private lostandfoudService: LostandfoudService){}
+  constructor(private lostandfoudService: LostandfoudService) {}
+  isLostFoundModalvisible : boolean=false;
   ngOnInit(): void {
     navigator.geolocation.getCurrentPosition((position) => {
-      this.lostandfoudService.changeData({latitude : position.coords.latitude, longitude : position.coords.longitude});
-});
- }
-
-}
+      this.lostandfoudService.changeData({latitude: position.coords.latitude,longitude: position.coords.longitude});
+    });
+    setTimeout(()=>{
+      this.isLostFoundModalvisible = true;
+    },2000)
+  }
+} 
